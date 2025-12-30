580eeb2aac63bd32af82ebc91eb978c56b56f3e0
Alexandre Beaufays <alexandre.beaufays@kraken.tech>
2025-12-30 21:36:34 +0100

    Change sample telescope keymap to custom ones

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -27,30 +27,34 @@ return {
             pcall(require('telescope').load_extension, 'fzf')
             pcall(require('telescope').load_extension, 'ui-select')
 
+            local function is_git_repo()
+                vim.fn.system('git rev-parse --is-inside-work-tree')
+                return vim.v.shell_error == 0
+            end
+
             -- See `:help telescope.builtin`
             local builtin = require 'telescope.builtin'
-            vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
-            vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-            vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-            vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-            vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-            vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-            vim.keymap.set('n', '<leader><leader>',
-                function()
-                    builtin.buffers {
-                        sort_lastused = true,
-                        ignore_current_buffer = true,
-                    }
-                end,
-                { desc = '[ ] Find existing buffers'})
 
-            -- It's also possible to pass additional configuration options.
-            --  See `:help telescope.builtin.live_grep()` for information about particular keys
-            vim.keymap.set('n', '<leader>s/', function()
-                builtin.live_grep {
-                    grep_open_files = true,
-                    prompt_title = 'Live Grep in Open Files',
-                }
-            end, { desc = '[S]earch [/] in Open Files' })
-        end,}
+            -- File navigation 
+            vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = '[F]ind [F]iles' })
+            vim.keymap.set('n', '<leader><leader>', function() builtin.buffers { sort_lastused = true, ignore_current_buffer = true, } end, { desc = '[ ] Find existing buffers'})
+
+            -- vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
+            -- vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+            -- vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
+
+            -- Config
+            vim.keymap.set('n', '<leader>fct', builtin.builtin, { desc = '[F]ind [C]onfig [T]elescope' })
+            vim.keymap.set('n', '<leader>fcc', function() builtin.colorscheme { enable_preview = true } end, { desc = '[F]ind [C]onfig [C]olorscheme' })
+
+            -- Git
+            if is_git_repo() then
+                vim.keymap.set('n', '<leader>fgb', builtin.git_branches, { desc = '[F]ind [G]it [B]ranch' })
+                -- vim.keymap.set('n', '<leader>fga', builtin.git_commits
+            end
+
+            -- Misc
+            vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = '[F]ind [H]elp' })
+        end,
+    }
 }
