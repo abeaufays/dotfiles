1cf312fd52ff093df98d6adade8c5628412da4b7
Alexandre Beaufays <alexandre.beaufays@kraken.tech>
2025-12-30 17:02:02 +0100

    Add various colorscheme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,4 +1,16 @@
 return {
+    { "EdenEast/nightfox.nvim" },
+    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+    { "rebelot/kanagawa.nvim" },
+    {
+        "baliestri/aura-theme",
+        config = function(plugin)
+            vim.opt.rtp:append(plugin.dir .. "/packages/neovim")
+        end
+    },
+    {
+        "Mofiqul/vscode.nvim"
+    },
     {
         'folke/tokyonight.nvim',
         lazy = false,
