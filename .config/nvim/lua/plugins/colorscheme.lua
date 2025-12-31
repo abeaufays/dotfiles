7116751774720b59a97b386da73a77e00ca47177
Alexandre Beaufays <alexandre.beaufays@kraken.tech>
2025-12-31 14:15:18 +0100

    Add github colorschemes and update config format

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,20 +1,22 @@
 return {
-    { "EdenEast/nightfox.nvim" },
-    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-    { "rebelot/kanagawa.nvim" },
+    { "EdenEast/nightfox.nvim", lazy = false, priority = 1000, },
+    { "catppuccin/nvim", name = "catppuccin", lazy=false, priority = 1000 },
+    { "rebelot/kanagawa.nvim", lazy = false, priority = 1000, },
+    { "baliestri/aura-theme", lazy = false, priority = 1000, config = function(plugin) vim.opt.rtp:append(plugin.dir .. "/packages/neovim") end },
+    { "Mofiqul/vscode.nvim", lazy = false, priority = 1000, },
     {
-        "baliestri/aura-theme",
-        config = function(plugin)
-            vim.opt.rtp:append(plugin.dir .. "/packages/neovim")
+        "navarasu/onedark.nvim", lazy = false, priority = 1000,
+        config = function()
+            require('onedark').setup {
+                style = 'darker'
+            }
         end
     },
     {
-        "Mofiqul/vscode.nvim"
+        'projekt0n/github-nvim-theme', name = 'github-theme', lazy = false, priority = 1000,
     },
     {
-        'folke/tokyonight.nvim',
-        lazy = false,
-        priority = 1000,
+        'folke/tokyonight.nvim', lazy = false, priority = 1000,
         config = function()
             vim.cmd([[colorscheme tokyonight-night]])
         end
