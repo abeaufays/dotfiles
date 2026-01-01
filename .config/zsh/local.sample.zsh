43ee666f28f5b082bdd5c4e11b4654029087dcb9
Alexandre Beaufays <beaufays.alexandre@gmail.com>
2026-01-01 03:03:49 +0100

    Update zsh/local.sample.zsh to avoid exec and nested sessions

--- .config/zsh/local.sample.zsh
+++ .config/zsh/local.sample.zsh
@@ -1,5 +1,6 @@
 # Open a set of tmux session
-if [ -x "$(command -v tmux)" ] && [ -z "${TMUX}" ]; then
-    tmux new-session -d -s config -c ~/.config >/dev/null 2>&1
-    exec tmux new-session -A -s main-project -c ~/projects/main-project/ >/dev/null 2>&1
+if [ -x "$(command -v tmux)" ] && [ -z "$TMUX" ]; then
+    tmux has-session -t config 2>/dev/null || tmux new -s config -c ~/.config -d
+    tmux has-session -t main 2>/dev/null || tmux new -s main -d
+    tmux attach -t main
 fi
