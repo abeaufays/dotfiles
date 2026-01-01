47185837c522006bdf6460280832721b57c4d441
Alexandre Beaufays <beaufays.alexandre@gmail.com>
2026-01-01 01:48:08 +0100

    Format telescope config

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -12,7 +12,7 @@ return {
                 end,
             },
             { 'nvim-telescope/telescope-ui-select.nvim' },
-            { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
+            { 'nvim-tree/nvim-web-devicons',            enabled = vim.g.have_nerd_font },
         },
         config = function()
             require('telescope').setup {
@@ -21,23 +21,25 @@ return {
                     ['ui-select'] = {
                         require('telescope.themes').get_dropdown(),
                     },
-                    ['fzf'] = {}
+                    ['fzf'] = {},
                 },
             }
             pcall(require('telescope').load_extension, 'fzf')
             pcall(require('telescope').load_extension, 'ui-select')
 
             local function is_git_repo()
-                vim.fn.system('git rev-parse --is-inside-work-tree')
+                vim.fn.system 'git rev-parse --is-inside-work-tree'
                 return vim.v.shell_error == 0
             end
 
             -- See `:help telescope.builtin`
             local builtin = require 'telescope.builtin'
 
-            -- File navigation 
+            -- File navigation
             vim.keymap.set('n', '<leader>f', builtin.find_files, { desc = 'Search [F]iles' })
-            vim.keymap.set('n', '<leader><leader>', function() builtin.buffers { sort_lastused = true, ignore_current_buffer = true, } end, { desc = '[ ] Find existing buffers'})
+            vim.keymap.set('n', '<leader><leader>', function()
+                builtin.buffers { sort_lastused = true, ignore_current_buffer = true }
+            end, { desc = '[ ] Find existing buffers' })
 
             -- vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
             -- vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
@@ -45,7 +47,9 @@ return {
 
             -- Config
             vim.keymap.set('n', '<leader>sct', builtin.builtin, { desc = '[T]elescope' })
-            vim.keymap.set('n', '<leader>scc', function() builtin.colorscheme { enable_preview = true } end, { desc = '[C]olorscheme' })
+            vim.keymap.set('n', '<leader>scc', function()
+                builtin.colorscheme { enable_preview = true }
+            end, { desc = '[C]olorscheme' })
 
             -- Git
             if is_git_repo() then
@@ -55,6 +59,7 @@ return {
 
             -- Misc
             vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[H]elp' })
+
         end,
-    }
+    },
 }
