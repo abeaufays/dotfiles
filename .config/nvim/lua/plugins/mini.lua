659a8c178c397f057c91b118e7cf049a380f4e82
Alexandre Beaufays <alexandre.beaufays@kraken.tech>
2026-01-06 18:37:19 +0100

    Add nvim plugin mini.move

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -5,6 +5,20 @@ return {
         config = function()
             local statusline = require 'mini.statusline'
             statusline.setup { use_icons = true }
-        end
-    }
+            require('mini.move').setup {
+                mappings = {
+                    -- Remapping to arrow keys as my tiling window manager is already mapped to <M-hjkl>
+                    left = '<M-Left>',
+                    right = '<M-Right>',
+                    down = '<M-Down>',
+                    up = '<M-Up>',
+
+                    line_left = '<M-Left>',
+                    line_right = '<M-Right>',
+                    line_down = '<M-Down>',
+                    line_up = '<M-Up>',
+                },
+            }
+        end,
+    },
 }
