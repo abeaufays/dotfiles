9ec13310c8cbebf89b941df9585157335ea78314
Alexandre Beaufays <alexandre.beaufays@kraken.tech>
2026-01-12 12:15:34 +0000

    Integrate my custom oil extension into my config

--- .config/nvim/lua/plugins/status-line.lua
+++ .config/nvim/lua/plugins/status-line.lua
@@ -3,8 +3,22 @@ return {
         'nvim-lualine/lualine.nvim',
         dependencies = { 'nvim-tree/nvim-web-devicons' },
         config = function()
+            local config = require 'lualine.config'
+            local sections = vim.deepcopy(config.get_config().sections)
+            sections.lualine_c = {
+                function()
+                    local ok, oil = pcall(require, 'oil')
+                    if ok then
+                        return vim.fn.fnamemodify(oil.get_current_dir(), ':~')
+                    else
+                        return ''
+                    end
+                end,
+            }
+
+            local oil_extension = { sections = sections, filetypes = { 'oil' } }
             require('lualine').setup {
-                extensions = { 'oil' },
+                extensions = { oil_extension },
             }
         end,
     },
