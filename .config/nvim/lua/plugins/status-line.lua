96b7d9fa3db9eb206ac593212099dac2f2c666aa
Alexandre Beaufays <alexandre.beaufays@kraken.tech>
2026-01-13 23:19:34 +0000

    Customize nvim status-line

--- .config/nvim/lua/plugins/status-line.lua
+++ .config/nvim/lua/plugins/status-line.lua
@@ -3,9 +3,19 @@ return {
         'nvim-lualine/lualine.nvim',
         dependencies = { 'nvim-tree/nvim-web-devicons' },
         config = function()
-            local config = require 'lualine.config'
-            local sections = vim.deepcopy(config.get_config().sections)
-            sections.lualine_c = {
+            local sections = {
+                lualine_a = { 'mode' },
+                lualine_b = { 'diagnostics' },
+                lualine_c = {
+                    { 'filename', path = 1, file_status = true, newfile_status = true },
+                },
+                lualine_x = {},
+                lualine_y = { 'diff', 'branch' },
+                lualine_z = { 'location' },
+            }
+
+            local oil_sections = vim.deepcopy(sections)
+            oil_sections.lualine_c = {
                 function()
                     local ok, oil = pcall(require, 'oil')
                     if ok then
@@ -16,9 +26,10 @@ return {
                 end,
             }
 
-            local oil_extension = { sections = sections, filetypes = { 'oil' } }
+            local oil_extension = { sections = oil_sections, filetypes = { 'oil' } }
             require('lualine').setup {
                 extensions = { oil_extension },
+                sections = sections,
             }
         end,
     },
