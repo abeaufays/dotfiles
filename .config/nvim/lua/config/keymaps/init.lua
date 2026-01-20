6e933979713246a77353e490cba7b60b9aa64b77
Alexandre Beaufays <alexandre.beaufays@kraken.tech>
2026-01-20 23:35:09 +0100

    Add a simple helper to import modules instead of classes in Python
    Made with AI

--- .config/nvim/lua/config/keymaps/init.lua
+++ .config/nvim/lua/config/keymaps/init.lua
@@ -1,4 +1,5 @@
 require 'config.keymaps.yank_filename'
+require 'config.keymaps.python_imports'
 
 vim.keymap.set('n', '<leader>w', '<cmd>update<cr>', { desc = 'Save' })
 vim.keymap.set('n', '<leader>q', '<cmd>q<cr>', { desc = 'Quit' })
