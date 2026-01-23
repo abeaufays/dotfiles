a98770578845153af28ee6ef14d16ddb2050de41
Alexandre Beaufays <beaufays.alexandre@gmail.com>
2026-01-24 00:12:30 +0100

    Change completion plugins to nvim-cmp

--- .config/nvim/lua/plugins/completion.lua
+++ .config/nvim/lua/plugins/completion.lua
@@ -1,47 +1,46 @@
 return {
-  'saghen/blink.cmp',
-  dependencies = { 'rafamadriz/friendly-snippets' },
-  version = '1.*',
-  ---@module 'blink.cmp'
-  ---@type blink.cmp.Config
-  opts = {
-    -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-    -- 'super-tab' for mappings similar to vscode (tab to accept)
-    -- 'enter' for enter to accept
-    -- 'none' for no mappings
-    --
-    -- All presets have the following mappings:
-    -- C-space: Open menu or open docs if already open
-    -- C-n/C-p or Up/Down: Select next/previous item
-    -- C-e: Hide menu
-    -- C-k: Toggle signature help (if signature.enabled = true)
-    --
-    -- See :h blink-cmp-config-keymap for defining your own keymap
-    keymap = { preset = 'enter' },
+    {
+        "hrsh7th/nvim-cmp",
+        lazy = false,
+        priority = 100,
+        dependencies = {
+            "onsails/lspkind.nvim",
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-path",
+            "hrsh7th/cmp-buffer",
+            { "L3MON4D3/LuaSnip", build = "make install_jsregexp" },
+            "saadparwaiz1/cmp_luasnip",
+        },
+        config = function()
+            local cmp = require "cmp"
 
-    appearance = {
-      -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-      -- Adjusts spacing to ensure icons are aligned
-      nerd_font_variant = 'mono'
-    },
+            cmp.setup {
+                sources = {
+                    { name = "nvim_lsp" },
+                    { name = "cody" },
+                    { name = "path" },
+                    { name = "buffer" },
+                },
+                mapping = {
+                    ["<down>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
+                    ["<up>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
+                    ["<cr>"] = cmp.mapping(
+                        cmp.mapping.confirm {
+                            behavior = cmp.ConfirmBehavior.Insert,
+                            select = true,
+                        },
+                        { "i", "c" }
+                    ),
+                    ["<left>"] = cmp.mapping.abort()
+                },
 
-    signature = { enabled = true },
-
-    -- (Default) Only show the documentation popup when manually triggered
-    completion = { documentation = { auto_show = false } },
-
-    -- Default list of enabled providers defined so that you can extend it
-    -- elsewhere in your config, without redefining it, due to `opts_extend`
-    sources = {
-      default = { 'lsp', 'path', 'snippets', 'buffer' },
-    },
-
-    -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
-    -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
-    -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
-    --
-    -- See the fuzzy documentation for more information
-    fuzzy = { implementation = "prefer_rust_with_warning" }
-  },
-  opts_extend = { "sources.default" }
+                -- Enable luasnip to handle snippet expansion for nvim-cmp
+                snippet = {
+                    expand = function(args)
+                        vim.snippet.expand(args.body)
+                    end,
+                },
+            }
+        end,
+    }
 }
