536d69f3308a8dbc9786bd2ec019ee10517aac4b
Alexandre Beaufays <alexandre.beaufays@kraken.tech>
2026-02-13 15:26:55 +0100

    Revert "Remove hop.nvim"
    This reverts commit 9f6434933ee4460ddbd3b95e91de46b5268fe6b2.

---
+++ .config/nvim/lua/plugins/hop.lua
@@ -0,0 +1,15 @@
+return {
+    'smoka7/hop.nvim',
+    version = '*',
+    config = function()
+        -- place this in one of your configuration file(s)
+        local hop = require 'hop'
+        hop.setup {
+            keys = 'sadfjklewcmpgh',
+        }
+
+        vim.keymap.set('', '<leader>j', function()
+            hop.hint_words {}
+        end, { remap = true })
+    end,
+}
