5a9a62347704ec940f244091db1a8c9dda845269
Alexandre Beaufays <alexandre.beaufays@kraken.tech>
2026-04-17 23:14:13 +0200

    Move snippets navigation out of tab to be on C-kj

--- .config/nvim/lua/plugins/luasnip.lua
+++ .config/nvim/lua/plugins/luasnip.lua
@@ -16,19 +16,15 @@ return {
             })
 
             -- Keybindings for snippet navigation
-            vim.keymap.set({ "i", "s" }, "<Tab>", function()
+            vim.keymap.set({ "i", "s" }, "<C-k>", function()
                 if luasnip.expand_or_jumpable() then
                     luasnip.expand_or_jump()
-                else
-                    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Tab>", true, false, true), "n", false)
                 end
             end, { silent = true })
 
-            vim.keymap.set({ "i", "s" }, "<S-Tab>", function()
+            vim.keymap.set({ "i", "s" }, "<C-j>", function()
                 if luasnip.jumpable(-1) then
                     luasnip.jump(-1)
-                else
-                    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<S-Tab>", true, false, true), "n", false)
                 end
             end, { silent = true })
 
