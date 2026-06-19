ec503cfbd9afa0b2d1061d630cac6dcec38044f4
Alexandre Beaufays <alexandre.beaufays@kraken.tech>
2026-06-19 21:16:32 +0200

    Move from dap-ui to dap-view

--- .config/nvim/lua/plugins/debugger.lua
+++ .config/nvim/lua/plugins/debugger.lua
@@ -4,14 +4,13 @@ return {
         dependencies = {
             'mason-org/mason.nvim',
             'jay-babu/mason-nvim-dap.nvim',
-            'nvim-neotest/nvim-nio',
-            'rcarriga/nvim-dap-ui',
+            'igorlfs/nvim-dap-view',
             'theHamsta/nvim-dap-virtual-text',
             'mfussenegger/nvim-dap-python',
         },
         keys = function(_, keys)
             local dap = require 'dap'
-            local dapui = require 'dapui'
+            local dapview = require 'dap-view'
             return {
                 {
                     '<F5>',
@@ -47,15 +46,14 @@ return {
                 },
                 {
                     '<F7>',
-                    dapui.toggle,
-                    desc = 'Debug: See last session result.',
+                    dapview.toggle,
+                    desc = 'Debug: Toggle debug view',
                 },
                 unpack(keys),
             }
         end,
         config = function()
             local dap = require 'dap'
-            local dapui = require 'dapui'
 
             require('mason-nvim-dap').setup {
                 -- Makes a best effort to setup the various debuggers with
@@ -72,25 +70,10 @@ return {
                     'debugpy',
                 },
             }
-            dapui.setup {
-                -- set icons to characters that are more likely to work in every terminal.
-                --    feel free to remove or use ones that you like more! :)
-                --    don't feel like these are good choices.
-                icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
-                controls = {
-                    icons = {
-                        pause = '⏸',
-                        play = '▶',
-                        step_into = '⏎',
-                        step_over = '⏭',
-                        step_out = '⏮',
-                        step_back = 'b',
-                        run_last = '▶▶',
-                        terminate = '⏹',
-                        disconnect = '⏏',
-                    },
-                },
+            require('dap-view').setup {
+                auto_toggle = true,
             }
+
             require('nvim-dap-virtual-text').setup()
             vim.api.nvim_set_hl(0, 'DapBreak', { fg = '#e51400' })
             vim.api.nvim_set_hl(0, 'DapStop', { fg = '#ffcc00' })
@@ -120,9 +103,15 @@ return {
                 vim.fn.sign_define(tp, { text = icon, texthl = hl, numhl = hl })
             end
 
-            dap.listeners.after.event_initialized['dapui_config'] = dapui.open
-            dap.listeners.before.event_terminated['dapui_config'] = dapui.close
-            dap.listeners.before.event_exited['dapui_config'] = dapui.close
+            -- This is useful to make sure I can step over while in the bottom right pane
+            dap.listeners.before.event_stopped['fix_winfixbuf'] = function()
+                for _, win in ipairs(vim.api.nvim_list_wins()) do
+                    if not vim.wo[win].winfixbuf then
+                        vim.api.nvim_set_current_win(win)
+                        return
+                    end
+                end
+            end
 
             require('dap-python').setup 'uv'
         end,
