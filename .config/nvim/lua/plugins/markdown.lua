86719312e831811bed287553c169227d7b2009c6
Alexandre Beaufays <alexandre.beaufays@kraken.tech>
2026-06-28 21:09:49 +0200

    Configure nvim markdown extenstion to still show comments
    This extension stylisize the buffer to show what the markdown will look
    like when rendered.
    Thus it will conceal comments.
    I still want to see comments when editing the file.

--- .config/nvim/lua/plugins/markdown.lua
+++ .config/nvim/lua/plugins/markdown.lua
@@ -6,6 +6,13 @@ return {
         dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
         ---@module 'render-markdown'
         ---@type render.md.UserConfig
-        opts = {},
+        opts = {
+            html = {
+                -- Turn off HTML comment concealing to show <!-- --> comments
+                comment = {
+                    conceal = false,
+                },
+            },
+        },
     },
 }
