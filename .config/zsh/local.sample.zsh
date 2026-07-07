a68d8435f95a153812d547282379ec93665463c3
Alexandre Beaufays <beaufays.alexandre@gmail.com>
2026-07-07 23:27:03 +0200

    Remove irrelevant zsh/local.sample.zsh
    I don't automatically startup tmux session anymore

--- .config/zsh/local.sample.zsh
+++
@@ -1,6 +0,0 @@
-# Open a set of tmux session
-if [ -x "$(command -v tmux)" ] && [ -z "$TMUX" ]; then
-    tmux has-session -t config 2>/dev/null || tmux new -s config -c ~/.config -d
-    tmux has-session -t main 2>/dev/null || tmux new -s main -d
-    tmux attach -t main
-fi
