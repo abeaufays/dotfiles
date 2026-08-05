16e862676903ecbc61a4125746927ffa310fca0a
Alexandre Beaufays <alexandre.beaufays@kraken.tech>
2026-08-05 22:08:05 +0200

    Add quick interface on quitting unsaved buffer in nvim

--- .config/nvim/lua/config/keymaps/init.lua
+++ .config/nvim/lua/config/keymaps/init.lua
@@ -9,7 +9,21 @@ vim.keymap.set('n', 'grp', python_imports.transform_python_class_import_to_modul
     { desc = 'Python [I]mports: class to modules' })
 
 vim.keymap.set('n', '<leader>w', '<cmd>update<cr>', { desc = 'Save' })
-vim.keymap.set('n', '<leader>q', '<cmd>q<cr>', { desc = 'Quit' })
+vim.keymap.set('n', '<leader>q', function()
+    if vim.bo.modified then
+        vim.ui.select(
+            { 'Save & quit', 'Quit without saving', 'Cancel' },
+            { prompt = 'Unsaved changes' },
+            function(choice)
+                if choice == 'Save & quit' then vim.cmd('wq')
+                elseif choice == 'Quit without saving' then vim.cmd('q!')
+                end
+            end
+        )
+    else
+        vim.cmd('q')
+    end
+end, { desc = 'Quit' })
 vim.keymap.set('n', '<leader><Tab>', '<cmd>wq<cr>', { desc = 'Save & Quit' })
 
 -- Make it easy to exit terminal mode
