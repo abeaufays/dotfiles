2bee4ced67380ad3b0cf605f439eadddb8ab27c6
Alexandre Beaufays <alexandre.beaufays@kraken.tech>
2026-08-31 22:04:38 +0200

    nvim: Remove sourcing shortcut
    I never used it, I would now use ZR

--- .config/nvim/lua/config/keymaps/init.lua
+++ .config/nvim/lua/config/keymaps/init.lua
@@ -29,14 +29,5 @@ vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 vim.keymap.set('v', '<', '<gv', { desc = 'Indent left and reselect' })
 vim.keymap.set('v', '>', '>gv', { desc = 'Indent right and reselect' })
 
--- Source keymaps (only in config directory)
-local cwd = vim.uv.cwd()
-local config_dir = vim.fn.expand '~/.config'
-if cwd and cwd:find(config_dir, 1, true) == 1 then
-    vim.keymap.set('n', '<leader>X', '<cmd>source %<CR>', { desc = 'Source file' })
-    vim.keymap.set('n', '<leader>x', ':.lua<CR>', { desc = 'Source line' })
-    vim.keymap.set('v', '<leader>x', ':lua<CR>', { desc = 'Source selection' })
-end
-
 -- Clear highlights on search when pressing <Esc> in normal mode
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
