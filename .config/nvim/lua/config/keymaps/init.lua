7745c14bb852cd0dc59caeb9e852379d3b104f2b
Alexandre Beaufays <alexandre.beaufays@kraken.tech>
2026-08-31 22:03:00 +0200

    nvim: Remove leader n to find partial words
    I made that to mimic a vscode feature but I'm now used to using f

--- .config/nvim/lua/config/keymaps/init.lua
+++ .config/nvim/lua/config/keymaps/init.lua
@@ -1,10 +1,6 @@
 require 'config.keymaps.yank_filename'
 local python_imports = require 'customs.python_imports'
 
-vim.keymap.set('n', '<leader>n', function()
-    vim.fn.setreg('/', '[A-Z_\\-]')
-    vim.cmd('normal n')
-end, { desc = '[N]ext partial word' })
 vim.keymap.set('n', 'grp', python_imports.transform_python_class_import_to_module,
     { desc = 'Python [I]mports: class to modules' })
 
