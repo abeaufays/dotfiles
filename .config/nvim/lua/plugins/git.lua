2024c03be7e908571e14c7215684faa087a3b445
Alexandre Beaufays <alexandre.beaufays@kraken.tech>
2026-09-04 23:40:29 +0200

    nvim: Add a ]c [c move in files to navigate git changes

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -4,7 +4,31 @@ return {
         'lewis6991/gitsigns.nvim',
         config = function()
             local gitsigns = require 'gitsigns'
-            gitsigns.setup {}
+            local repeatable_move = require 'nvim-treesitter-textobjects.repeatable_move'
+
+            gitsigns.setup {
+                on_attach = function(bufnr)
+                    local function map(lhs, rhs, desc)
+                        vim.keymap.set('n', lhs, rhs, { buffer = bufnr, desc = desc })
+                    end
+
+                    local repeatable_hunk_move = repeatable_move.make_repeatable_move(function(opts)
+                        if vim.wo.diff then
+                            vim.cmd.normal { opts.forward and ']c' or '[c', bang = true }
+                        else
+                            gitsigns.nav_hunk(opts.forward and 'next' or 'prev')
+                        end
+                    end)
+
+                    map(']c', function()
+                        repeatable_hunk_move { forward = true }
+                    end, 'Next change')
+
+                    map('[c', function()
+                        repeatable_hunk_move { forward = false }
+                    end, 'Previous change')
+                end,
+            }
 
             -- Configs
             vim.keymap.set('n', '<leader>gcb', gitsigns.toggle_current_line_blame, { desc = 'toggle [B]lame' })
